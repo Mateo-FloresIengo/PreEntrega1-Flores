8ee3971c70efccbc91e893f8f3fb444bd4879cc1
Mateo Flores Iengo <mfloresiengo@gmail.com>
2024-05-02 20:21:23 +0200

    script modificado segunda entrega

--- js/script.js
+++ js/script.js
@@ -1,85 +1,93 @@
-let precioPizza = 10000
-let precioBebida = 2000
-let precioPostre = 5000
+//MENSAJE BIENVENIDA
+let mensajeBienvenida = alert("Bienvenido/a a Pizzeria El Jefe");
+//FUNCION PARA ACTUALIZAR EL MENSAJE DE BIENVENIDA
+function actualizarMensaje() {
+    let horaActual = new Date().getHours();
+    console.log(horaActual);
+    let mensaje;
+
+    if(horaActual < 12) {
+        mensaje = "¡Buenos dias! Hace tu pedido para disfrutar del mejor sabor de nuestras pizzas";
+    } else if(horaActual < 19) {
+        mensaje = "¡Buenas tardes! Hace tu pedido para disfrutar del mejor sabor de nuestras pizzas";
+    } else {
+        mensaje = "¡Buenas noches! Hace tu pedido para disfrutar del mejor sabor de nuestras pizzas";
+    }
+    alert(mensaje);
+}
+actualizarMensaje();
 
 function correr(){
-    
-    let pedidoPizza = Number(prompt("¿Cuántas pizzas desea pedir?"))
-    let buclePizza = 0
-    
-    if(pedidoPizza>0){
-    
-        while(buclePizza!=4){
-            buclePizza = Number(prompt("Marque con un numero el sabor de la Pizza. Para finalizar, marque 4: 1.Muzzarella - 2.Napolitana - 3.Provolone - 4.Finalizar"))
-        
-            switch(buclePizza){
-                case 1: document.write("Usted seleccionó: Muzzarella <br>");
-                break;
-                case 2: document.write("Usted seleccionó: Napolitana <br>");
-                break;
-                case 3: document.write("Usted seleccionó: Provolone <br>");
-                break;
-                case 4: document.write(`Cantidad de pizzas seleccionadas: ${pedidoPizza} <br> El precio de cada pizza es: $${precioPizza} <br><br>`);
-            }
-        
+    // CLASE PARA CREAR PRODUCTOS
+    class Producto {
+        constructor(nombre, precio){
+            this.nombre = nombre;
+            this.precio = precio;
         }
-    }
-    else{
-        document.write("No desea pedir pizza.<br><br>")
-    }
-    
-    let pedidoBebida = Number(prompt("¿Cuantas bebidas desea pedir?"))
-    let bucleBebida = 0
-    
-    if(pedidoBebida>0){
-    
-        while(bucleBebida!=4){
-            bucleBebida = Number(prompt("Marque con un numero la bebida que desea pedir. Para finalizar, marque 4: 1.Agua - 2.Gaseosa - 3.Cerveza - 4.Finalizar"))
-        
-            switch(bucleBebida){
-                case 1: document.write("Usted seleccionó: Agua <br>");
-                break;
-                case 2: document.write("Usted seleccionó: Gaseosa <br>");
-                break;
-                case 3: document.write("Usted seleccionó: Cerveza <br>");
-                break;
-                case 4: document.write(`Cantidad de bebidas seleccionadas: ${pedidoBebida} <br> El precio de cada bebida es: $${precioBebida} <br><br>`);
-            }
-        
-        }
-    }
-    else{
-        document.write(" <br> No desea pedir bebidas.<br><br>")
-    }
-    
-    let pedidoPostre = Number(prompt("¿Cuántos postres desea pedir?"))
-    let buclePostre = 0
-    
-    if(pedidoPostre>0){
-    
-        while(buclePostre!=4){
-            buclePostre = Number(prompt("Marque con un numero el postre que desea pedir. Para finalizar, marque 4: 1.Helado - 2.Torta - 3.Flan - 4.Finalizar"))
-        
-            switch(buclePostre){
-                case 1: document.write("<br> Usted seleccionó: Helado <br>");
-                break;
-                case 2: document.write("Usted seleccionó: Torta <br>");
-                break;
-                case 3: document.write("Usted seleccionó: Flan <br>");
-                break;
-                case 4: document.write(`Cantidad de postres seleccionadas: ${pedidoPostre} <br> El precio de cada postre es: $${precioPostre} <br><br>`);
-            }
-        
-        }
-    }
-    else{
-        document.write("<br> No desea pedir postre.<br><br>")
-    }
-    
-    let calculoPizza = pedidoPizza * precioPizza
-    let calculoBebida = pedidoBebida * precioBebida
-    let calculoPostre = pedidoPostre * precioPostre
-    let precioFinal = calculoPizza + calculoBebida + calculoPostre
-    document.write(`<br> El precio final de su pedido es: $${precioFinal}`)
-    
-}
+    };
+
+    let producto1 = new Producto("Muzzarela", 100);
+    let producto2 = new Producto("Napolitana", 125);
+    let producto3 = new Producto("Provolone", 150);
+    let producto4 = new Producto("Agua", 30);
+    let producto5 = new Producto("Gaseosa", 40);
+    let producto6 = new Producto("Cerveza", 50);
+    let producto7 = new Producto("Helado", 60);
+    let producto8 = new Producto("Torta", 70);
+    let producto9 = new Producto("Flan", 80);
+
+    let productos = [producto1, producto2, producto3, producto4, producto5, producto6, producto7, producto8, producto9];
+
+    let mensaje = "";
+    // Recorrer el array de productos y agregar la información de cada producto al string
+    productos.forEach(producto => {
+        mensaje += `Producto: ${producto.nombre} - Precio: $${producto.precio}\n`;
+    });
+    // Mostrar la información de todos los productos en un alert
+    alert(`Nuestra carta:\n${mensaje}`);
+
+    // Definir un array de carrito para almacenar los productos
+    let carrito = [];
+
+    let pedidoPizza = prompt(`Ingrese el sabor de la pizza que desea pedir:\n- ${producto1.nombre}\n- ${producto2.nombre}\n- ${producto3.nombre}`);
+    let precioPizza = Number(prompt("Ingrese el precio del producto:"));
+
+    let nuevoProducto = {
+        nombre: pedidoPizza,
+        precio: parseFloat(precioPizza)
+    };
+    carrito.push(nuevoProducto);
+
+    let pedidoBebida = prompt(`Ingrese la bebida que desea pedir:\n- ${producto4.nombre}\n- ${producto5.nombre}\n- ${producto6.nombre}`);
+    let precioBebida = Number(prompt("Ingrese el precio del producto:"));
+    let nuevoProducto2 = {
+        nombre: pedidoBebida,
+        precio: parseFloat(precioBebida)
+    };
+    carrito.push(nuevoProducto2);
+
+    let pedidoPostre = prompt(`Ingrese el postre que desea pedir:\n- ${producto7.nombre}\n- ${producto8.nombre}\n- ${producto9.nombre}`);
+    let precioPostre = Number(prompt("Ingrese el precio del producto:"));
+    let nuevoProducto3 = {
+        nombre: pedidoPostre,
+        precio: parseFloat(precioPostre)
+    };
+    carrito.push(nuevoProducto3);
+
+
+    let total = 0;
+    let mensajeCarrito = "";
+
+    // Recorrer el array carrito y agregar los datos de cada producto al mensajeCarrito
+    carrito.forEach(producto => {
+        mensajeCarrito += `Producto: ${producto.nombre} - Precio: $${producto.precio}\n`;
+    });
+    // Recorre el array "carrito" y suma los precios de los productos
+    carrito.forEach(producto => {
+        total += parseFloat(producto.precio);
+    });
+
+    // Muestra los productos del carrito y el total del pedido al usuario
+    document.write(`Productos en el carrito:<br>${mensajeCarrito}<br><br>`);
+    document.write(`El total del pedido es: $${total}`);
+};
