f2c5cd4e2576716f29d0579a409ad146118caa14
Mateo Flores Iengo <mfloresiengo@gmail.com>
2024-06-02 14:37:47 +0200

    proyecto finalizado

--- js/script.js
+++ js/script.js
@@ -1,83 +1,255 @@
+// Obtengo elementos con DOM
+const cardsContainer = document.getElementById("cards-container");
+const verCarrito = document.getElementById("ver-carrito");
+const carritoContainer = document.getElementById("carrito-container");
+const cantidadCarrito = document.getElementById("cantidadCarrito");
 
-// Funcion a ejecutar cuando el usuario realiza el pedido
-function guardarPedido() {
-    // Obtener los valores de los campos del formulario
-    const nombreUsuario = document.getElementById("nombre").value;
-    const telefonoUsuario = document.getElementById("telefono").value;
-    const pizzaSeleccionada = document.getElementById("selector-pizza").value;
-    const bebidaSeleccionada = document.getElementById("selector-bebida").value;
-    const postreSeleccionado = document.getElementById("selector-postre").value;
+let carrito = JSON.parse(localStorage.getItem("carrito")) || [];
 
-    // Crear un objeto con los datos capturados
-    const pedido = {
-        nombre: nombreUsuario,
-        telefono: telefonoUsuario,
-        pizza: pizzaSeleccionada,
-        bebida: bebidaSeleccionada,
-        postre: postreSeleccionado
-    };
+// Capturo los productos del json mediante fetch
+const obtenerProductos = async () => {
+    const response = await fetch("data.json");
+    const data = await response.json();
 
-    // Convertir el objeto a formato JSON
-    const pedidoJSON = JSON.stringify(pedido);
 
-    // Guardar el JSON en localStorage:
-    localStorage.setItem("pedido", pedidoJSON);
- 
-}
-
-// Funcion a ejecutar cuando el usuario realiza el pedido
-function mostrarPedido () {
-
-    // Recuperar la cadena JSON del localStorage
-    const pedidoJSON = localStorage.getItem("pedido");
-
-    // Convierte la cadena JSON a un objeto
-    const pedido = JSON.parse(pedidoJSON);
-
-    // Vincular la variable con la lista del HTML
-    const lista = document.getElementById("lista-pedido");
-
-    // Crear los items de la lista con los datos captados
-    const pizzaItem = document.createElement("li");
-    pizzaItem.innerText = `Pizza: ${pedido.pizza}`;
-
-    const bebidaItem = document.createElement("li");
-    bebidaItem.innerText = `Bebida: ${pedido.bebida}`;
-
-    const postreItem = document.createElement("li");
-    postreItem.innerText = `Postre: ${pedido.postre}`;
-
-    //Agregar los items a la lista
-    lista.appendChild(pizzaItem);
-    lista.appendChild(bebidaItem);
-    lista.appendChild(postreItem);
+    // Recorro el json de productos para mostralos en un div
+    data.forEach((producto) => {
+        // Creo el div donde van a estar los productos
+        let content = document.createElement("div");
+        content.className = "cards";
+        content.innerHTML = `
+        <img src="${producto.img}">
+        <h3>${producto.nombre}</h3>
+        <p>$ ${producto.precio}</p>
+        `;
     
-}
+        cardsContainer.append(content);
+    
+        // Creo el boton para agregar productos al carrito
+        let botonCard = document.createElement("button");
+        botonCard.innerText = "Añadir al carrito";
+        botonCard.className = "boton-card";
+    
+        content.append(botonCard);
+  
+        // Evento para agregar los productos al carrito
+        botonCard.addEventListener("click", () => {
+        
+            const repetir = carrito.some((repetirProducto) => repetirProducto.id === producto.id);
+          
+            if (repetir) {
+                carrito.map((prod) => {
+                    if(prod.id === producto.id){
+                        prod.cantidad++;
+                    };
+                });
+            } else {
+                carrito.push({
+                    id: producto.id,
+                    img: producto.img,
+                    nombre: producto.nombre,
+                    precio: producto.precio,
+                    cantidad: producto.cantidad,
+                });
+            };
 
-// Evento que se ejecuta cuando el usuario hace click en realizar pedido
-document.getElementById("form-pedido").addEventListener("submit", (event) => {
-    event.preventDefault();
-    guardarPedido();
-    mostrarPedido();
-});
 
-// Funcion a ejecutar cuando el usuario finaliza el pedido
-function finalizarPedido () {
-    const nombreUsuario = document.getElementById("nombre").value;
-    const mensajeFinal = document.getElementById("mensaje-final");
-    const mensaje = document.createElement("p");
-    mensaje.innerText = `Muchas gracias ${nombreUsuario} por su pedido. Nos pondremos en contacto a la brevedad para entregarle sus productos.`
-    mensajeFinal.appendChild(mensaje);
-    mensaje.style.cssText = "margin: 10px ; font-size: 1.5rem ; font-style: italic";
-}
+           Toastify({
+                text: "Producto añadido",
+                duration: 3000,
+                style: {
+                   background: "linear-gradient(to right, #00b09b, #96c93d)",
+                }
+            }).showToast();
+                  
+            contadorCarrito();
+            guardarLocal();
+        });
+    });
 
-// Funcion para limpiar el localStorage una vez finalizado el pedido
+
+};
+
+
+obtenerProductos();
+
+// Funcion para ver el carrito
+const mostrarCarrito = () => {
+
+
+    carritoContainer.innerHTML = "";
+    carritoContainer.style.display = "flex";
+
+
+    // Creo el div donde se van a mostrar los productos en el carrito
+    const carritoHeader = document.createElement("div");
+    carritoHeader.className = "carrito-header";
+    carritoHeader.innerHTML = `
+        <h1 class="titulo-carrito">Carrito</h1>
+    `;
+
+
+    carritoContainer.append(carritoHeader);
+
+
+    // Creo el boton para cerrar el carrito
+    const cuadroBoton = document.createElement("button");
+    cuadroBoton.innerText = "X";
+    cuadroBoton.className = "btn btn-light";
+
+
+    // Evento para cerrar el carrito
+    cuadroBoton.addEventListener("click", () => {
+        carritoContainer.style.display = "none";
+    });
+
+
+    carritoHeader.append(cuadroBoton);
+
+
+    // Recorro el carrito para mostrar los productos que tiene
+    carrito.forEach((producto) => {
+        // Creo el div donde se van a mostrar los productos del carrito
+        let carritoContent = document.createElement("div");
+        carritoContent.className = "carrito-content";
+        carritoContent.innerHTML = `
+            <img class="img-carrito" src="${producto.img}">
+            <h3 class="h3-carrito">${producto.nombre}</h3>
+            <p class="p-carrito">$ ${producto.precio}</p>
+            <span id="restar" class="btn btn-success"> - </span>
+            <p class="p-carrito">Cantidad: ${producto.cantidad}</p>
+            <span id="sumar" class="btn btn-success"> + </span>
+            <p class="p-carrito">Total: ${producto.cantidad * producto.precio}</p>
+            <span class="btn btn-danger"> X </span>
+        `;
+
+
+        carritoContainer.append(carritoContent);
+
+
+        // Obtengo el boton de restar producto del carrito
+        let restar = carritoContent.querySelector("#restar");
+
+
+        restar.addEventListener("click", () => {
+            if (producto.cantidad !== 1) {
+                producto.cantidad--;
+            };
+            guardarLocal();
+            mostrarCarrito();
+        });
+
+
+        // Obtengo el boton de sumar producto del carrito
+        let sumar = carritoContent.querySelector("#sumar");
+
+
+        sumar.addEventListener("click", () => {
+            producto.cantidad++;
+            guardarLocal();
+            mostrarCarrito();
+        });
+  
+
+
+        // Obtengo el boton para eliminar producto del carrito
+        let botonEliminar = carritoContent.querySelector(".btn-danger");
+
+
+        // Evento para eliminar producto del carrito
+        botonEliminar.addEventListener("click", () => {
+            eliminarProducto(producto.id);
+        });
+
+
+    });
+
+
+    // Metodo para calcular el total de los productos del carrito
+    const total = carrito.reduce((acc, el) => acc + el.precio * el.cantidad, 0);
+
+
+    // Creo un div para mostrar el total de la compra
+    const totalCompra = document.createElement("div");
+    totalCompra.className = "total-compra";
+    totalCompra.innerHTML = `Total a pagar: $${total}`;
+    carritoContainer.append(totalCompra);
+
+
+    // Creo un boton de finalizar compra
+    const finalizarCompra = document.createElement("button");
+    finalizarCompra.className = "btn btn-outline-secondary";
+    finalizarCompra.innerText = "Finalizar compra";
+    carritoContainer.append(finalizarCompra);
+
+
+    finalizarCompra.addEventListener("click", () => {
+        Swal.fire({
+            title: "Compra Finalizada!",
+            text: "Muchas gracias por elegirnos!",
+            icon: "success",
+            confirmButtonText: "Volver a comprar"
+        });
+
+
+        limpiarStorage();
+    });
+};
+
+
+// Evento para mostrar el carrito
+verCarrito.addEventListener("click", mostrarCarrito);
+
+
+
+
+// Funcion para eliminar un producto del carrito
+const eliminarProducto = (id) => {
+    const foundId = carrito.find((element) => element.id === id);
+
+
+    console.log(foundId);
+
+
+    carrito = carrito.filter((carritoId) => {
+        return carritoId !== foundId;
+    });
+
+
+    contadorCarrito();
+    guardarLocal();
+    mostrarCarrito();
+};
+
+
+// Funcion para mostrar el numero de productos en el carrito
+const contadorCarrito = () => {
+   cantidadCarrito.style.display = "block";
+
+
+   const carritoLength = carrito.length;
+
+
+   localStorage.setItem("carritoLength", JSON.stringify(carritoLength));
+
+
+   cantidadCarrito.innerText = JSON.parse(localStorage.getItem("carritoLength"));
+};
+
+
+// Funcion para guardar elementos en LocalStorage
+const guardarLocal = () => {
+   localStorage.setItem("carrito", JSON.stringify(carrito));
+};
+
+
+contadorCarrito();
+
+
+// Funcion para limpiar el localStorage y el carrito una vez finalizado el pedido
 function limpiarStorage() {
-    localStorage.clear();
-}
-
-// Evento a ejectutar cuando el usuario hace click en finalizar pedido
-document.getElementById("finalizar-pedido").addEventListener("click", function(e) {
-    finalizarPedido();
-    limpiarStorage();
-});
+   localStorage.clear();
+   carrito = JSON.parse(localStorage.getItem("carrito")) || [];
+   carritoContainer.style.display = "none";
+   contadorCarrito();
+};
